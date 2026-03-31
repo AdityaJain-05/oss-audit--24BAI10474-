06e326a5ccaa525c3c48d1694205efb0e49c402c
AdityaJain-05 <aj6762064@gmail.com>
2026-03-31 23:50:45 +0530

    Update and rename manifesto.sh.txt to manifesto.sh

---
+++ manifesto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "===== Open Source Manifesto ====="
+
+read -p "Enter your name: " NAME
+read -p "Your favorite open source tool: " TOOL
+
+echo ""
+echo "My name is $NAME."
+echo "I believe open source software encourages collaboration and innovation."
+echo "One of my favorite open source tools is $TOOL."
+echo "I support the philosophy of sharing knowledge freely."
